2ddd650bfdd8dbaf8d55faaf7735959199e9cdee
imad <imadelfahssi@gmail.com>
2020-12-10 22:05:15 +0100

    Añadido de comentarios

--- src/main/java/com/mycompany/racionalalbertogarzonimad/Racional.java
+++ src/main/java/com/mycompany/racionalalbertogarzonimad/Racional.java
@@ -10,30 +10,32 @@ package com.mycompany.racionalalbertogarzonimad;
  * @author alberto
  */
 public class Racional {
-
+    //Atributos privados numerador y denomirador
     private int numerador;
     private int denominador;
-
+    //Constructor por defecto, indicando como numerador y denominador el valor 1.
     public Racional() {
         this.numerador = 1;
         this.denominador = 1;
     }
+//Constructor parametrizado. 
 
     public Racional(int numerador, int denominador) {
         this.numerador = numerador;
-
+        //Controlamos que si el denominador no es cero, en tal caso asignamos el valor a 1.
         if (denominador == 0) {
             denominador = 1;
         }
         this.denominador = denominador;
 
     }
+// Método imprimirConsola(), que imprimirá por consola el número racional
 
     public void imprimirConsola() {
 
         System.out.println("El numero racional es: " + numerador + "/" + denominador);
     }
-
+//Método suma(Racional x),  sumará al racional que llama al método el valor del número racional que recibe como argumento. 
     public void suma(Racional x) {
         if (this.denominador == x.denominador) {
 
@@ -48,7 +50,7 @@ public class Racional {
         }
 
     }
-
+//Método producto(Racional x), que guardará en los atributos del objeto que hace la llamada la siguiente información:
     public void producto(Racional x) {
         this.numerador = this.numerador * x.numerador;
         this.denominador = this.denominador * x.denominador;
@@ -64,6 +66,7 @@ public class Racional {
         }
         
     }
+   //metodos Get
     public int getNumerador() {
         return numerador;
     }
@@ -71,7 +74,7 @@ public class Racional {
     public int getDenominador() {
         return denominador;
     }
-
+    //metodos Set
     public void setNumerador(int numerador) {
         this.numerador = numerador;
     }
@@ -82,7 +85,7 @@ public class Racional {
         }
         this.denominador = denominador;
     }
-
+//Método toString(), que devolverá una cadena de texto con el siguiente formato "a/b".
     @Override
     public String toString() {
         return "Racional{" + numerador / denominador + '}';
