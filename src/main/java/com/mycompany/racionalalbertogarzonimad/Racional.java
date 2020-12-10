2980e14d17b8e63be8ab25f25cd275e3b55fd9be
Alberto Garzón Rodríguez <albertogr578@gmail.com>
2020-12-10 12:53:00 +0100

    Crea la clase Racional y añade sus atributos

---
+++ src/main/java/com/mycompany/racionalalbertogarzonimad/Racional.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.racionalalbertogarzonimad;
+
+/**
+ *
+ * @author alberto
+ */
+public class Racional {
+
+    private int enteroA;
+    private int enteroB;
+
+}
