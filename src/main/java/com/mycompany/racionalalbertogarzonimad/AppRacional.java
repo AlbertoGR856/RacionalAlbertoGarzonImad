ee227321a5b8b974e4a4e4f62cafa42f13197b8b
Alberto <albertogr578@gmail.com>
2020-12-11 13:43:14 +0100

    Imprime el resultado de la división de los racionales K y Z, almacenando el resultado en un nuevo objeto de tipo Racional

--- src/main/java/com/mycompany/racionalalbertogarzonimad/AppRacional.java
+++ src/main/java/com/mycompany/racionalalbertogarzonimad/AppRacional.java
@@ -22,21 +22,23 @@ public class AppRacional {
         Racional prueba1 = new Racional();
 
         prueba1 = Racional.aleatorio();
-        
-            //Imprimo los tres racionales y limitamos la salida a dos decimales.
-       System.out.printf("Racionalz: %.2f \n", (float)racionalZ.getNumerador() / racionalZ.getDenominador());
-       System.out.printf("Racionalk: %.2f \n", (float)racionalK.getNumerador() / racionalK.getDenominador());
-        System.out.printf("Aleatorio prueba1: %.2f \n", (float)prueba1.getNumerador() / prueba1.getDenominador());
-        
-        
-        
+
+        //Imprimo los tres racionales y limitamos la salida a dos decimales.
+        System.out.printf("Racionalz: %.2f \n", (float) racionalZ.getNumerador() / racionalZ.getDenominador());
+        System.out.printf("Racionalk: %.2f \n", (float) racionalK.getNumerador() / racionalK.getDenominador());
+        System.out.printf("Aleatorio prueba1: %.2f \n", (float) prueba1.getNumerador() / prueba1.getDenominador());
+
         //Realizo la suma
-          racionalZ.suma(racionalK);
-        System.out.println("El resultado de la suma es: "+racionalZ.toString());
-          
-          //Comparamos los dos racionales, comprobando su igualdad
+        racionalZ.suma(racionalK);
+        System.out.println("El resultado de la suma es: " + racionalZ.toString());
+
+        //Comparamos los dos racionales, comprobando su igualdad
         System.out.println(Racional.igualdad(racionalZ, racionalK));
-        
+
+        //Se muestra la division de los dos racionales, guardando el resultado de la división en un nuevo objeto de tipo Racional
+        Racional resultadoDivision = Racional.division(racionalZ, racionalK);
+        System.out.println("El resultado de dividir Z y K es" + resultadoDivision);
+
     }
 
 }
